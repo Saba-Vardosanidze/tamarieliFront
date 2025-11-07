841e749e37afd815f06d98430089c522112fc011
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-11-07 15:09:07 +0400

    Update HeroSection.tsx

--- features/landing/components/primitives/HeroSection.tsx
+++ features/landing/components/primitives/HeroSection.tsx
@@ -1,11 +1,12 @@
 import { getTranslations } from 'next-intl/server';
+import tamarieli from '../../../../public/images/headerImages/png/tamarieli.png';
 
 const HeroSection = async () => {
   const t = await getTranslations('HeroSection');
 
   return (
     <div className="flex justify-center items-center bg-white w-full">
-      <div className="flex flex-col items-center gap-[40px] bg-[length:100%_100%] bg-[url('/images/headerImages/png/tamarieli.png')] bg-no-repeat bg-center m-auto w-full max-w-[1440px] min-h-[323px] lg:min-h-[804px]">
+      <div className={`flex flex-col items-center gap-[40px] bg-[length:100%_100%] bg-[url('${tamarieli}')] bg-no-repeat bg-center m-auto w-full max-w-[1440px] min-h-[323px] lg:min-h-[804px]`}>
         <div className="relative w-full max-w-[1440px] h-full">
           <div className="top-[80px] lg:top-[163px] left-[16px] lg:left-[102px] absolute flex flex-col gap-[16px] lg:gap-[42px]">
             <p className="w-full max-w-[410px] font-extralight text-[#000000] text-[26px] lg:text-[56px] lg:leading-[75px] tracking-[0%] custom-firago-font">
