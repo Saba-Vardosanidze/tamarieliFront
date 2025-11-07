f6e0f0915965986055f4e017b63b90e049dbf23f
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-11-07 15:24:50 +0400

    Update HeroSection.tsx

--- features/landing/components/primitives/HeroSection.tsx
+++ features/landing/components/primitives/HeroSection.tsx
@@ -6,7 +6,10 @@ const HeroSection = async () => {
 
   return (
     <div className="flex justify-center items-center bg-white w-full">
-      <div className={`flex flex-col items-center gap-[40px] bg-[length:100%_100%] bg-[url('${tamarieli}')] bg-no-repeat bg-center m-auto w-full max-w-[1440px] min-h-[323px] lg:min-h-[804px]`}>
+      <div
+        className="flex flex-col items-center gap-[40px] bg-no-repeat bg-center bg-cover m-auto w-full max-w-[1440px] min-h-[323px] lg:min-h-[804px]"
+        style={{ backgroundImage: `url(${tamarieli.src})` }}
+      >
         <div className="relative w-full max-w-[1440px] h-full">
           <div className="top-[80px] lg:top-[163px] left-[16px] lg:left-[102px] absolute flex flex-col gap-[16px] lg:gap-[42px]">
             <p className="w-full max-w-[410px] font-extralight text-[#000000] text-[26px] lg:text-[56px] lg:leading-[75px] tracking-[0%] custom-firago-font">
