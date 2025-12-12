72ffd11947675e28f0dfd8d810857302af3c4147
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-12-12 04:58:31 +0400

    Update HeroSection image and adjust styles
    Replaced the HeroSection background image with a new 'heroSection.webp' file and updated related import. Adjusted layout styles, including min-height, text sizes, and positioning for improved appearance.

--- features/landing/components/primitives/HeroSection.tsx
+++ features/landing/components/primitives/HeroSection.tsx
@@ -1,5 +1,5 @@
 import { getTranslations } from 'next-intl/server';
-import tamarieli from '../../../../public/Images/headerImages/png/tamarieli.png';
+import tamarieli from '../../../../public/Images/headerImages/png/heroSection.webp';
 
 const HeroSection = async () => {
   const t = await getTranslations('HeroSection');
@@ -7,15 +7,15 @@ const HeroSection = async () => {
   return (
     <div className="flex justify-center items-center bg-white w-full">
       <div
-        className="flex flex-col items-center gap-[40px] bg-cover bg-no-repeat bg-center m-auto w-full max-w-[1440px] min-h-[323px] lg:min-h-[804px]"
+        className="flex flex-col items-center gap-[40px] bg-cover bg-no-repeat bg-center m-auto w-full max-w-[1440px] min-h-[323px] lg:min-h-[515px]"
         style={{ backgroundImage: `url(${tamarieli.src})` }}
       >
         <div className="relative w-full max-w-[1440px] h-full">
-          <div className="top-[80px] lg:top-[163px] left-[16px] lg:left-[102px] absolute flex flex-col gap-[16px] lg:gap-[42px]">
-            <p className="w-full max-w-[410px] font-extralight text-[#000000] text-[26px] lg:text-[56px] lg:leading-[75px] tracking-[0%] custom-firago-font">
+          <div className="top-[50px] lg:top-[53px] left-[16px] lg:left-[102px] absolute flex flex-col gap-[16px] lg:gap-[42px]">
+            <p className="w-full max-w-[410px] font-extralight text-[#000000] text-[26px] lg:text-[46px] lg:leading-[75px] tracking-[0%] custom-firago-font">
               {t('welcomeText')}
             </p>
-            <p className="flex flex-col w-full max-w-[473px] font-extralight text-[#000000] text-[16px] lg:text-[26px] italic leading-[100%] tracking-[0%] custom-firago-font">
+            <p className="flex flex-col w-full max-w-[473px] font-extralight text-[#000000] text-[14px] lg:text-[20px] italic leading-[100%] tracking-[0%] custom-firago-font">
               <span>{t('inspiration')}</span>
               <span>{t('identity')}</span>
             </p>
