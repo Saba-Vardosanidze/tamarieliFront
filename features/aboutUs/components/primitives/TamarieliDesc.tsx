30f730b6435e4be226e7f04cac04e0ac5e87aa23
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-12-21 16:54:57 +0400

    feature/less z index about us page

--- features/aboutUs/components/primitives/TamarieliDesc.tsx
+++ features/aboutUs/components/primitives/TamarieliDesc.tsx
@@ -1,56 +1,38 @@
-"use client";
+'use client';
 
-import { useTranslations } from "next-intl";
-import Image from "next/image";
+import { useTranslations } from 'next-intl';
+import Image from 'next/image';
 
 const TamarieliDesc = () => {
-  const t = useTranslations("TamarieliDesc");
+  const t = useTranslations('TamarieliDesc');
 
   return (
-    <div
-      className="
-        relative flex justify-center items-center mx-auto
-        w-full
-        lg:min-h-[1320px] md:min-h-[500px] min-h-[400px]
-        bg-[url('/images/headerImages/png/about-us-tamarieli.png')]
-        bg-cover bg-center bg-no-repeat
-      "
-    >
-      <div
-        className="
-          absolute
-          lg:block
-         right-4 top-0
-          lg:right-[50px] lg:top-[150px]
-        "
-      >
+    <div className="relative flex justify-center items-center bg-[url('/images/headerImages/png/about-us-tamarieli.png')] bg-cover bg-no-repeat bg-center mx-auto w-full min-h-[400px] md:min-h-[500px] lg:min-h-[1320px]">
+      <div className="lg:block top-0 lg:top-[150px] right-4 lg:right-[50px] absolute">
         <Image
           src="/images/headerImages/png/tamarieliLogo.png"
           alt="logo"
           width={150}
           height={150}
-          className="
-            w-[70px] h-[70px]
-            lg:w-[150px] lg:h-[150px]
-          "
+          className="w-[70px] lg:w-[150px] h-[70px] lg:h-[150px]"
         />
       </div>
 
       <div className="absolute inset-0" />
 
-      <div className="relative z-10 flex lg:flex-row flex-col justify-between items-center lg:items-stretch w-full max-w-[1440px] lg:px-0 md:px-6 px-4">
-        <div className="flex flex-col justify-center items-center lg:gap-7 md:gap-5 gap-4 mx-auto lg:px-4 md:px-3 px-2 w-full text-center">
-          <h3 className="font-bold text-[#000000] lg:text-[56px] md:text-[28px] sm:text-[24px] text-[38px]">
-            {t("title")}
+      <div className="z-[9] relative flex lg:flex-row flex-col justify-between items-center lg:items-stretch px-4 md:px-6 lg:px-0 w-full max-w-[1440px]">
+        <div className="flex flex-col justify-center items-center gap-4 md:gap-5 lg:gap-7 mx-auto px-2 md:px-3 lg:px-4 w-full text-center">
+          <h3 className="font-bold text-[#000000] text-[38px] sm:text-[24px] md:text-[28px] lg:text-[56px]">
+            {t('title')}
           </h3>
 
-          <ul className="text-[#000000] lg:text-[44px] md:text-[24px] sm:text-[20px] text-[18px] list-disc list-inside">
-            <li>{t("list.inspiration")}</li>
-            <li>{t("list.identity")}</li>
+          <ul className="text-[#000000] text-[18px] sm:text-[20px] md:text-[24px] lg:text-[44px] list-disc list-inside">
+            <li>{t('list.inspiration')}</li>
+            <li>{t('list.identity')}</li>
           </ul>
 
-          <p className="lg:mt-5 md:mt-4 mt-3 max-w-[900px] text-[#000000] lg:text-[44px] md:text-[24px] sm:text-[20px] text-[18px] lg:leading-normal md:leading-relaxed leading-relaxed">
-            {t("paragraph")}
+          <p className="mt-3 md:mt-4 lg:mt-5 max-w-[900px] text-[#000000] text-[18px] sm:text-[20px] md:text-[24px] lg:text-[44px] leading-relaxed md:leading-relaxed lg:leading-normal">
+            {t('paragraph')}
           </p>
         </div>
       </div>
