f19b569e43d7261245498bd9c23d74685ca9d273
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-12-30 22:58:14 +0400

    bugfix/paypal

--- features/landing/components/primitives/Paypal.tsx
+++ features/landing/components/primitives/Paypal.tsx
@@ -1,13 +1,17 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { Copy, Check, Send } from 'lucide-react';
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Copy, Check, Send } from "lucide-react";
+import { useTranslations } from "next-intl";
 
 const Paypal = () => {
+  const t = useTranslations("paypalTexts");
+
   const [copied, setCopied] = useState(false);
-  const textToCopy = 'FR76 1027 8010 8800 0226 3180 179';
+  const textToCopy = "FR76 1027 8010 8800 0226 3180 179";
+  const textToCopy2 = "tamarielitamarieli@gmail.com";
 
   const handleCopy = async () => {
     try {
@@ -15,7 +19,17 @@ const Paypal = () => {
       setCopied(true);
       setTimeout(() => setCopied(false), 2000);
     } catch (err) {
-      console.error('Failed to copy:', err);
+      console.error("Failed to copy:", err);
+    }
+  };
+
+  const handleCopy2 = async () => {
+    try {
+      await navigator.clipboard.writeText(textToCopy2);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    } catch (err) {
+      console.error("Failed to copy:", err);
     }
   };
 
@@ -25,38 +39,17 @@ const Paypal = () => {
         <div className="flex lg:flex-row flex-col justify-between items-center gap-16 lg:gap-20">
           <div className="flex flex-col gap-8 order-2 lg:order-1 w-full lg:w-[500px]">
             <h2 className="font-medium text-[21px] text-gray-900 text-center">
-              თქვენი თანადგომა ჩვენთვის ძალიან მნიშვნელოვანია
+              {t("title")}
             </h2>
 
             <div className="flex flex-col items-center gap-6 w-full">
-              <Link
-                href="https://www.paypal.com/ge/home"
-                className="flex justify-center items-center gap-4 bg-[#0070BA] hover:bg-[#005ea6] shadow-md px-8 py-4 rounded-lg w-full transition-colors"
-              >
-                <Image
-                  src="/Images/headerImages/svg/PayPal_Logo.svg"
-                  alt="paypal"
-                  width={50}
-                  height={50}
-                />
-                <span className="font-semibold text-white text-lg md:text-xl">
-                  PayPal
-                </span>
-              </Link>
-
-              <div className="flex items-center gap-4 my-2 w-full">
-                <div className="flex-1 border-gray-500 border-t"></div>
-                <span className="font-medium text-gray-700 text-sm">ან</span>
-                <div className="flex-1 border-gray-500 border-t"></div>
-              </div>
-
               <div className="flex flex-col gap-4 bg-white shadow-md p-8 rounded-xl w-full">
                 <p className="font-medium text-gray-700 text-sm text-center">
-                  საბანკო გადარიცხვა (IBAN)
+                  {t("bankTransferTitle")}
                 </p>
 
                 <div
-                  onClick={handleCopy}
+                  onClick={handleCopy2}
                   className="flex items-center gap-4 hover:bg-gray-50 p-4 border border-gray-200 rounded-lg transition-colors cursor-pointer"
                 >
                   <p className="flex-1 font-mono text-[14px] text-gray-900 md:text-lg text-center">
@@ -72,7 +65,57 @@ const Paypal = () => {
 
                 {copied && (
                   <p className="font-medium text-green-600 text-sm text-center">
-                    დაკოპირდა
+                    {t("copied")}
+                  </p>
+                )}
+              </div>
+
+              <div className="flex items-center gap-4 my-2 w-full">
+                <div className="flex-1 border-gray-500 border-t"></div>
+                <span className="font-medium text-gray-700 text-sm">
+                  {t("or")}
+                </span>
+                <div className="flex-1 border-gray-500 border-t"></div>
+              </div>
+
+              <div className="flex flex-col gap-4 bg-white shadow-md p-8 rounded-xl w-full">
+                <p className="font-medium text-gray-700 text-sm text-center">
+                  {t("paypalTransferTitle")}
+                </p>
+
+                <div
+                  onClick={handleCopy}
+                  className="flex items-center gap-4 hover:bg-gray-50 p-4 border border-gray-200 rounded-lg transition-colors cursor-pointer"
+                >
+                  <p className="flex-1 font-mono text-[14px] text-gray-900 md:text-lg text-center">
+                    {textToCopy2}
+                  </p>
+
+                  {copied ? (
+                    <Check className="flex-shrink-0 w-5 h-5 text-green-600" />
+                  ) : (
+                    <Copy className="flex-shrink-0 w-5 h-5 text-gray-400" />
+                  )}
+                </div>
+
+                <Link
+                  href="https://www.paypal.com/ge/home"
+                  className="flex justify-center items-center gap-4 bg-[#0070BA] hover:bg-[#005ea6] shadow-md px-8 py-4 rounded-lg w-full transition-colors"
+                >
+                  <Image
+                    src="/Images/headerImages/svg/PayPal_Logo.svg"
+                    alt="paypal"
+                    width={50}
+                    height={50}
+                  />
+                  <span className="font-semibold text-white text-lg md:text-xl">
+                    Paypal
+                  </span>
+                </Link>
+
+                {copied && (
+                  <p className="font-medium text-green-600 text-sm text-center">
+                    {t("copied")}
                   </p>
                 )}
               </div>
