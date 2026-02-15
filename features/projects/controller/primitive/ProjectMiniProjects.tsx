df7881bbb832229263532f5204ac78898434b1de
Saba-Vardosanidze <sabavardo674@gmail.com>
2026-02-15 19:00:19 +0400

    feature/isLoading

--- features/projects/controller/primitive/ProjectMiniProjects.tsx
+++ features/projects/controller/primitive/ProjectMiniProjects.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { FaChevronRight } from 'react-icons/fa';
 import Pagination from './Pagination';
+import { MiniProjectsSkeleton } from './MiniProjectsSkeleton';
 
 type Props = {
   id: string;
@@ -28,51 +29,54 @@ export default function ProjectMiniProjects({ id, locale, t }: Props) {
 
   const lang = locale as 'en' | 'fr' | 'ka';
 
-  if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>Error...</p>;
-  if (!miniProjects || miniProjects.length === 0) {
-    return <p className="text-gray-400 text-center">Something was wrong</p>;
-  }
 
   return (
     <div className="flex flex-col gap-[20px]">
-      <div className="gap-8 grid sm:grid-cols-2 lg:grid-cols-3">
-        {miniProjects.map((eachElement) => (
-          <div
-            key={eachElement._id}
-            className="flex flex-col justify-between bg-white px-[10px] py-[10px] rounded-2xl"
-          >
-            <div className="flex flex-col">
-              <div className="relative h-56">
-                <Image
-                  src={eachElement.projectPicture}
-                  alt={eachElement.projectName[lang]}
-                  fill
-                  className="object-cover"
-                />
-              </div>
-              <div className="flex flex-col mt-[10px]">
-                <h4 className="font-bold text-black text-lg">
-                  {eachElement.projectName[lang].length > 50
-                    ? eachElement.projectName[lang].slice(0, 50) + '...'
-                    : eachElement.projectName[lang]}
-                </h4>
-                <p className="text-gray-500 text-sm leading-relaxed">
-                  {eachElement.projectDescription[lang].length > 100
-                    ? eachElement.projectDescription[lang].slice(0, 100) + '...'
-                    : eachElement.projectDescription[lang]}
-                </p>
-              </div>
-            </div>
-            <Link
-              href={`/${locale}/miniprojects/${eachElement._id}`}
-              className="flex items-center gap-2 mt-4 text-blue-600"
+      {isLoading ? (
+        <MiniProjectsSkeleton />
+      ) : !miniProjects || miniProjects.length === 0 ? (
+        <div>Something was wrong</div>
+      ) : (
+        <div className="gap-8 grid sm:grid-cols-2 lg:grid-cols-3">
+          {miniProjects.map((eachElement) => (
+            <div
+              key={eachElement._id}
+              className="flex flex-col justify-between bg-white px-[10px] py-[10px] rounded-2xl"
             >
-              {t('views')} <FaChevronRight size={12} />
-            </Link>
-          </div>
-        ))}
-      </div>
+              <div className="flex flex-col">
+                <div className="relative h-56">
+                  <Image
+                    src={eachElement.projectPicture}
+                    alt={eachElement.projectName[lang]}
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+                <div className="flex flex-col mt-[10px]">
+                  <h4 className="font-bold text-black text-lg">
+                    {eachElement.projectName[lang].length > 50
+                      ? eachElement.projectName[lang].slice(0, 50) + '...'
+                      : eachElement.projectName[lang]}
+                  </h4>
+                  <p className="text-gray-500 text-sm leading-relaxed">
+                    {eachElement.projectDescription[lang].length > 100
+                      ? eachElement.projectDescription[lang].slice(0, 100) +
+                        '...'
+                      : eachElement.projectDescription[lang]}
+                  </p>
+                </div>
+              </div>
+              <Link
+                href={`/${locale}/miniprojects/${eachElement._id}`}
+                className="flex items-center gap-2 mt-4 text-blue-600"
+              >
+                {t('views')} <FaChevronRight size={12} />
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
       {pagination && (
         <Pagination
           currentPage={page}
